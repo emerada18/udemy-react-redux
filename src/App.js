45f725553f6443d41a527bed16dcb37c16570dd5
emerada18 <emerada18@gmail.com>
2021-10-02 21:06:05 +0900

    create component and functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
-function App() {
-	return <div>Hello</div>;
-}
+import React from 'react';
+
+// function App() {
+// const greeting = 'Hello';
+// const dom = <h1>Hi</h1>;
+// const dom = <h1 className="foo">{greeting}</h1>;
+// return dom;
+// return (
+// return (
+// <div>
+// 	<label htmlFor="bar">bar</label>
+// 	<input
+// 		type="text"
+// 		onChange={() => {
+// 			console.log('I am clicked');
+// 		}}
+// 	/>
+// </div>
+// 	<React.Fragment>
+// 		<label htmlFor="bar">bar</label>
+// 		<input
+// 			type="text"
+// 			onChange={() => {
+// 				console.log('I am clicked');
+// 			}}
+// 		/>
+// 	</React.Fragment> //divを出力しないため
+// );
+// }
+
+const App = () => {
+	return (
+		<div>
+			<Cat />
+			<Cat />
+		</div>
+	);
+};
+
+const Cat = () => {
+	return (
+		<div>
+			<div>hi</div>
+		</div>
+	);
+};
 
 export default App;
